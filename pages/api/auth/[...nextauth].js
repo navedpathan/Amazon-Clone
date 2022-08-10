ce4dc29720665cc43d55cc645049eeb8cb725d2e
Naved Pathan <navedpathan646@gmail.com>
2022-08-10 16:20:45 +0530

    amzn commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,4 +13,9 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+    encryption: true,
+  },
 })
