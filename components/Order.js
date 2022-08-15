779bd297f0c52816d092b11454e312f437bfda8f
Naved Pathan <navedpathan646@gmail.com>
2022-08-16 00:11:19 +0530

    amzn commit

--- components/Order.js
+++ components/Order.js
@@ -6,12 +6,12 @@ const Order = ({ id, amount, amountShipping, items, timestamp, images }) => {
     <div className="relative border rounded-md">
         <div className="flex items-center space-x-10 p-5 bg-gray-100 text-sm text-gray-600">
             <div>
-                <p className="font-bold text-xs">ORDER PLACED</p>
+                <p className="font-bold text-xs pt-2">ORDER PLACED</p>
                 <p>{moment.unix(timestamp).format("DD MM YYYY")}</p>
             </div>
 
             <div>
-                <p className="text-xs font-bold">TOTAL</p>
+                <p className="text-xs font-bold pt-2">TOTAL</p>
                 <p>
                 <Currency quantity={amount} currency="INR" /> - Next Day Delivery{" "}
                 <Currency quantity={amountShipping} currency="INR" />
@@ -20,7 +20,7 @@ const Order = ({ id, amount, amountShipping, items, timestamp, images }) => {
 
             <p className="text-sm whitespace-nowrap sm:text-xl self-end flex-1 text-right text-blue-500">{items.length} items</p>
 
-            <p className="absolute top-2 right-2 w-49 lg:w-72 truncate text-xs whitespace-nowrap">ORDER # {id}</p>
+            <p className="absolute top-2 right-2 w-64 lg:w-80 truncate text-xs whitespace-nowrap">ORDER # {id}</p>
         </div>
 
         <div className="p-5 sm:p-10">
